6898b7c7d2df4625e402f1c55ebfc0ef55c46758
Amanda Morais <amanda.dgmorais@gmail.com>
2025-03-18 11:43:20 -0300

    Criação de uma função para adicionar os nomes

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 let listaNomes = [];
+
+function adicionarAmigo(){
+    let nomes = document.getElementById('amigo');
+    let nome = nomes.value;
+    let lista = document.getElementById('listaAmigos');
+
+    if (nome == ''){
+        alert("Por favor, insira um nome.");
+        return;
+    }
+    listaNomes.push(nome);
+        nomes.value = '';
+}
